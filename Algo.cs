ee4a5438de5e3cb80ef2657796ea4af4a7e1bdc3
Argee Abraham <aabraham@blharbert.com>
2020-08-10 15:16:47 -0500

    pizzaalgo comment number 1

---
+++ Algo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TestUI
+{
+    public class Algo
+    {
+        public Algo()
+        {
+
+            /* 
+             1. Decide which business to order from
+             2. Decide which type of pizza to order
+             */
+       
+        }
+    }
+}
